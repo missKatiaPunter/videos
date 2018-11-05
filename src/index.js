193234f70cdda7120d908ed2b92e6becc63c56a9
Katia <katiapunter@gmail.com>
2018-11-05 13:21:02 +0000

    tries to hide api key

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SearchBar from './components/search_bar';
+import config from './config'
+const akey = config.REACT_APP_KEY;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: akey };
+    console.log(this.state);
+  }
+  render() {
+    console.log(process.env.REACT_APP_KEY);
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
